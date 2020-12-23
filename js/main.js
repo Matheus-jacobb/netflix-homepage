252e4db1e7e6004bf90d88e4c22a1196152257ee
Matheus <matheusbendel@hotmail.com>
2020-12-23 19:40:56 -0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function inicialModal (modalID){
+    const modal = document.getElementById(modalID);
+    modal.classList.add('mostrar');
+    modal.addEventListener('click', (e) => {
+    if(e.target.id == modalID || e.target.className == 'fechar')
+        modal.classList.remove('mostrar');
+    });
+}
+
+
+const logo = document.querySelector ('#botao-info');
+logo.addEventListener('click' , () => inicialModal('modal-popup'));
+
+//https://www.youtube.com/watch?v=fu-enUG2VEE
+
+
+function redirecionarTrailer(){
+    window.location.href = "https://www.youtube.com/watch?v=2-W6_6gJda0";
+}
